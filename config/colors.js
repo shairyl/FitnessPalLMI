1fe4bfc42eee31766fdfebf3f84c4cd575960f29
Saveer29 <saveergovindu29@gmail.com>
2023-12-19 14:12:45 +1300

    add buttons, update colors

--- config/colors.js
+++ config/colors.js
@@ -1,6 +1,8 @@
 export default {
   black: "#000000",
   dark: "#333333",
+  darkGrey: "#4e4e4e",
+  green: "#00FF63",
   white: "#ffffff",
   grey: "#7b7b7b",
 };
--- screens/signupSurvey/SurveyScreen4.js
+++ screens/signupSurvey/SurveyScreen4.js
@@ -2,17 +2,23 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 
 import Screen from "../../components/Screen";
+import AppButton from "../../components/AppButton";
 
-function SurveyScreen4(props) {
+function SurveyScreen4({ navigation }) {
   return (
     <Screen style={styles.container}>
-      <Text>fdsfdfsfdsa</Text>
+      <AppButton
+        title="NEXT"
+        color="grey"
+        textColor="dark"
+        onPress={() => navigation.navigate("SurveyScreen1")}
+      />
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { padding: 20 },
+  container: { justifyContent: "flex-end", padding: 20 },
 });
 
 export default SurveyScreen4;
